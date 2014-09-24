7ec92f4b548bb383664cd5b4679fbfd5bc5babf8
Matteo Merola <mattmezza@gmail.com>
2014-09-24 16:41:01 +0200

    Url Encoding now works as it should

--- application/controllers/hello.php
+++ application/controllers/hello.php
@@ -1,6 +1,6 @@
 <?php
 
-$klein->respond('GET', '/hello/[a:name]', function ($request, $response, $service, $app) use ($klein) {
+$klein->respond('GET', '/hello/[:name]', function ($request, $response, $service, $app) use ($klein) {
 	$name = $request->name;
-	echo $klein->template->render('hello.twig', \Fleet\Utils::addAppInfoTo(array('name' => $name)));
+	echo $klein->template->render('hello.twig', \Fleet\Utils::addAppInfoTo(array('name' => rawurldecode($name))));
 });
